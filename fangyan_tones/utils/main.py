b94df8a1383c869fd62beba5427c8dbeb7f86fda
Wyatt Lansford <wyatt.lansford@heronsystems.com>
2021-09-16 21:01:33 -0400

    hacky way to test

--- fangyan_tones/utils/main.py
+++ fangyan_tones/utils/main.py
@@ -1,12 +1,10 @@
-from typing import String
-
 from pypinyin import pinyin as to_pinyin
 from pypinyin import Style as style
 
 tone_styles = [style.TONE, style.TONE2, style.TONE3]
 
 
-def char_to_pinyin(char: String, tone_style: int) -> String:
+def char_to_pinyin(char: str, tone_style: int) -> str:
     """Converts a single character to pinyin
     # TODO support heteronyms
 
@@ -25,7 +23,7 @@ def char_to_pinyin(char: String, tone_style: int) -> String:
     # Is created as a list of lists, so return as just string
     pinyin = to_pinyin(char, style=tone_styles[tone_style], heteronyms=False)[0][0]
 
-def chars_to_pinyin(chars: String, tone_style: int, as_list=True) -> List[String]:
+def chars_to_pinyin(chars: str, tone_style: int, as_list=True) -> [str]:
     chars_list = to_pinyin(chars, style=tone_style)
 
     if as_list:
@@ -33,3 +31,11 @@ def chars_to_pinyin(chars: String, tone_style: int, as_list=True) -> List[String
 
     # Return as space separated sentence
     return chars_list.join(" ")
+
+def load_and_convert():
+    user_input = input()
+    print(chars_to_pinyin, 0)
+
+
+if __name__ == "__main__":
+    load_and_convert()
