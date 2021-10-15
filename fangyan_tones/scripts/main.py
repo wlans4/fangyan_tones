95e86795df2a37638d8960205b0b1ab5c1778314
crown523 <kxlider@rocketmail.com>
2021-10-14 21:53:04 -0400

    start table stuff

--- fangyan_tones/scripts/main.py
+++ fangyan_tones/scripts/main.py
@@ -10,11 +10,22 @@ from fangyan_tones.utils import (
     filter_all_non_chinese_text
 )
 
+def create_table():
+    return 0
 
 def load_and_convert():
-    user_input = input()
-    print(filter_chinese_specific_punctuation(chars_to_pinyin(user_input, 1)))
+    curInput = []
+    while True:
+        try:
+            user_input = input()
+            if user_input == "Analyze":
+                create_table()
+            else:
+                curInput += chars_to_pinyin(user_input, 1)
+                print(curInput)
 
+        except EOFError:
+            break
 
 if __name__ == "__main__":
     load_and_convert()
