f99af4681a0f938a66a1c0d6e3e672adea86b52c
crown523 <kxlider@rocketmail.com>
2021-10-15 11:53:55 -0400

    add tone table creation functionality, update readme

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ To setup the project, run
 ```pip install .``` or ``` pip install -e .``` for an editable version
 
 You should then be able to run main.py in scripts to pass in chinese text
+
+After passing in the song lyrics, by inputting "Analyze" you can output the Tone table for the line endings.
--- fangyan_tones/scripts/main.py
+++ fangyan_tones/scripts/main.py
@@ -10,21 +10,33 @@ from fangyan_tones.utils import (
     filter_all_non_chinese_text
 )
 
-def create_table():
-    return 0
+def create_table(line_endings):
+    table = dict.fromkeys(['1', '2', '3', '4', 'Neutral', 'Total'], 0)
+    for char in line_endings:
+        # print(char)
+        if char[-1].isdigit():
+            table[char[-1]] += 1
+        else:
+            table['Neutral'] += 1
+    table['Total'] = len(line_endings)
+    return table
 
 def load_and_convert():
     curPinyin = []
+    line_endings = []
     while True:
         try:
             user_input = input()
             if user_input == "Analyze":
-                create_table()
+                print(create_table(line_endings))
             else:
                 cleaned_input = filter_chinese_specific_punctuation(user_input)
                 pinyin = chars_to_pinyin(cleaned_input, 2, as_list=True)
                 curPinyin += pinyin
-                print(curPinyin)
+                if pinyin:
+                    line_endings.append(pinyin[-1])
+                # print(curPinyin)
+                # print(line_endings)
         except EOFError:
             break
 
